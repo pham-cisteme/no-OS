8f24c5089b7901e4d9800e80adffa7bd2f34b854
Radu Sabau <radu.sabau@analog.com>
2023-09-29 12:13:27 +0300

    drivers: digital-io: max149x6 : Move driver files for MAX14906.
    Move max14906 driver files to max149x6
    Rename desc and init_param structures to max149x6 instead of max14906
    Add fault, ready and synch GPIO.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- drivers/digital-io/max149x6/max149x6-base.h
+++ drivers/digital-io/max149x6/max149x6-base.h
@@ -57,31 +57,37 @@
 /**
  * @brief Initialization parameter for the MAX149X6 device.
  */
-struct max14906_init_param {
+struct max149x6_init_param {
 	uint32_t chip_address;
 	struct no_os_spi_init_param *comm_param;
 	struct no_os_gpio_init_param *en_gpio_param;
+	struct no_os_gpio_init_param *fault_gpio_param;
+	struct no_os_gpio_init_param *ready_gpio_param;
+	struct no_os_gpio_init_param *synch_gpio_param;
 	bool crc_en;
 };
 
 /**
  * @brief Device descriptor for MAX149X6.
  */
-struct max14906_desc {
+struct max149x6_desc {
 	uint32_t chip_address;
 	struct no_os_spi_desc *comm_desc;
 	struct no_os_gpio_desc *en_gpio;
+	struct no_os_gpio_desc *fault_gpio;
+	struct no_os_gpio_desc *ready_gpio;
+	struct no_os_gpio_desc *synch_gpio;
 	uint8_t buff[MAX149X6_FRAME_SIZE + 1];
 	bool crc_en;
 };
 
 /** Write the value of a device register */
-int max149x6_reg_write(struct max14906_desc *, uint32_t, uint32_t);
+int max149x6_reg_write(struct max149x6_desc *, uint32_t, uint32_t);
 
 /** Read the value of a device register */
-int max149x6_reg_read(struct max14906_desc *, uint32_t, uint32_t *);
+int max149x6_reg_read(struct max149x6_desc *, uint32_t, uint32_t *);
 
 /** Update the value of a device register */
-int max149x6_reg_update(struct max14906_desc *, uint32_t, uint32_t, uint32_t);
+int max149x6_reg_update(struct max149x6_desc *, uint32_t, uint32_t, uint32_t);
 
 #endif
